43eeb8182c20034ed5b3b8610dd7d3bf6fc4edd9
smk-141510228 <141510228@smkassalaambandung.sch.id>
2017-02-21 16:45:45 +0700

    Hari Pertama Ujikom

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="col-10-0">
+            <div class=" panel-default">
+                <div class="panel-heading">Welcome</div>
+   
+    </div>
+</div>
+@endsection
